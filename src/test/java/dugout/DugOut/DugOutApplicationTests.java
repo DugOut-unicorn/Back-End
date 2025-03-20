0201352502cf3a050388e22351246c4e26526237
fbwougr121 <fbwogur121@naver.com>
2025-03-20 14:30:58 +0900

    first commit

---
+++ src/test/java/dugout/DugOut/DugOutApplicationTests.java
@@ -0,0 +1,13 @@
+package dugout.DugOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
