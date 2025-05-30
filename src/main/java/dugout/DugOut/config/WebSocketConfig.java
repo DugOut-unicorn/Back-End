637ffcea00bd4b22df558aac235b090c4d97b53f
정성훈 <modestnature@naver.com>
2025-05-31 01:26:54 +0900

    websocketconfig interceptor 수정

--- src/main/java/dugout/DugOut/config/WebSocketConfig.java
+++ src/main/java/dugout/DugOut/config/WebSocketConfig.java
@@ -5,7 +5,13 @@ import dugout.DugOut.repository.UserRepository;
 import dugout.DugOut.service.JwtService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -69,5 +75,27 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .withSockJS();
     }
 
-    // 이전에 쓰던 CONNECT interceptor는 이제 필요 없습니다.
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+                // 클라이언트가 CONNECT 프레임 보낼 때
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    // STOMP CONNECT 헤더에서 Authorization 꺼내기
+                    String authHeader = accessor.getFirstNativeHeader("Authorization");
+                    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                        String rawJwt = authHeader.substring(7);
+                        Integer userId = jwtService.getUserIdFromToken(rawJwt);
+                        // Principal 을 userId 로 설정
+                        accessor.setUser(() -> userId.toString());
+                        System.out.println("▶ CONNECT interceptor set Principal=" + userId);
+                    }
+                }
+                return message;
+            }
+        });
+    }
+
 }
