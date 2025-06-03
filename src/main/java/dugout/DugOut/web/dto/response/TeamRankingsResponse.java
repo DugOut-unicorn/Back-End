0d3d49e20b18ac9415c06a4ad6761b8c62f1f548
정성훈 <modestnature@naver.com>
2025-06-03 15:52:18 +0900

    최종 순위 api 수정

--- src/main/java/dugout/DugOut/web/dto/response/TeamRankingsResponse.java
+++ src/main/java/dugout/DugOut/web/dto/response/TeamRankingsResponse.java
@@ -5,23 +5,25 @@ import lombok.Getter;
 
 @Getter
 public class TeamRankingsResponse {
-
     private final Integer id;
     private final String teamName;
     private final Integer rank;
+    private final Integer currentRank;
 
-    public TeamRankingsResponse(Integer id, String teamName, Integer rank) {
+    public TeamRankingsResponse(Integer id, String teamName, Integer rank, Integer currentRank) {
         this.id = id;
         this.teamName = teamName;
         this.rank = rank;
+        this.currentRank = currentRank;
     }
 
     /** 엔티티 → DTO 변환 헬퍼 메서드 */
-    public static TeamRankingsResponse of(TeamRankings entity) {
+    public static TeamRankingsResponse of(TeamRankings entity, Integer currentRank) {
         return new TeamRankingsResponse(
                 entity.getId(),
                 entity.getTeamName(),
-                entity.getRank()
+                entity.getRank(),
+                currentRank
         );
     }
 }
