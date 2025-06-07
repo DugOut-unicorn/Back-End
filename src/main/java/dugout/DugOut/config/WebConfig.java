c7cb4c4a14746d1e4c5f20915604f1820380a0c1
정성훈 <modestnature@naver.com>
2025-06-07 15:52:26 +0900

    patch 메서드 추가

--- src/main/java/dugout/DugOut/config/WebConfig.java
+++ src/main/java/dugout/DugOut/config/WebConfig.java
@@ -31,7 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
                     "http://localhost:5173",  // Vite 개발 서버
                     "https://dev.dug-out.store"  // 개발 서버
                 )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .exposedHeaders("Authorization")  // JWT 토큰을 담은 Authorization 헤더 노출
                 .allowCredentials(true)  // 쿠키, 인증 헤더 등을 포함한 요청 허용
